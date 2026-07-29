1013911b9c9c538e159bda943bed61fec48d595f
Pranav Toshniwal <toshniwalpranav.jul@gmail.com>
2026-07-29 21:54:51 +0530

    add project

---
+++ src/test/java/testrunner/TestRunner.java
@@ -0,0 +1,24 @@
+package testrunner;
+
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"src/test/resources/AppFeatures"},
+		glue = {"stepdefinitions"},
+		plugin = {"pretty",
+				 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+				
+		
+		)
+
+
+
+public class TestRunner {
+
+}
---
+++ src/test/resources/AppFeatures/print.feature
@@ -0,0 +1,7 @@
+Feature: Print
+
+Scenario Outline: Print
+
+Given user is on webpage
+When user print
+Then close the page
